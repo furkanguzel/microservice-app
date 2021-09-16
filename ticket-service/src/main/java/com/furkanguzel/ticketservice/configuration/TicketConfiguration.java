f18e397c22575982f563e51037c6f898b37ab0f8
furkanguzel <furkanguzel0@gmail.com>
2021-09-17 00:56:20 +0300

    elasticsearch implementation for ticket microservice

--- ticket-service/src/main/java/com/furkanguzel/ticketservice/configuration/TicketConfiguration.java
+++ ticket-service/src/main/java/com/furkanguzel/ticketservice/configuration/TicketConfiguration.java
@@ -2,7 +2,13 @@ package com.furkanguzel.ticketservice.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@EnableJpaRepositories("com.furkanguzel")
+@EnableElasticsearchRepositories
 public class TicketConfiguration {
 
     @Bean
